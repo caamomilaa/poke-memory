4652591f3f583878fae72e09df4c2360831fd353
caamomilaa <c.velazquezdamico@gmail.com>
2025-04-29 12:41:38 +0200

    update

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+const gameContainerElement = document.getElementById('game-container');
+//event.target y ver a que hijo le hago click para añadir la clase de rotar
+
+//array vacio que lleno con numeros randoms para elegir imagenes
+
+let pokemons = [];
+
+const generatePokemons = () => {
+	while (pokemons.length < 6) {
+		const randomNumbers = Math.floor(Math.random() * 151 + 1);
+		if (!pokemons.includes(randomNumbers)) {
+			pokemons.push(randomNumbers);
+		}
+	}
+	pokemons = [...pokemons, ...pokemons];
+	pokemons.sort(() => Math.random() - 0.5);
+};
+
+const createCard = () => {
+	generatePokemons();
+	const fragment = document.createDocumentFragment();
+	pokemons.forEach(pokemon => {
+		//DIV CARD
+		const cardDiv = document.createElement('div');
+		cardDiv.classList.add('card');
+		//CARD FRONT
+		const cardFrontDiv = document.createElement('div');
+		cardFrontDiv.classList.add('card-front');
+		cardFrontDiv.style.setProperty(
+			'--poke-img',
+			`url(../assets/images/${pokemon}.png)`
+		);
+		cardDiv.append(cardFrontDiv);
+
+		//CARD BACK
+		const cardBackDiv = document.createElement('div');
+		cardBackDiv.classList.add('card-back');
+		cardDiv.append(cardBackDiv);
+		fragment.append(cardDiv);
+	});
+	gameContainerElement.append(fragment);
+};
+createCard();
+
+const showCards = () => {
+	for (let i = 0; i < pokemons.length; i++) {
+		gameContainerElement.children[i].classList.add('card-clicked');
+	}
+};
+showCards();
+
+const hideCards = setTimeout(() => {
+	for (let i = 0; i < pokemons.length; i++) {
+		gameContainerElement.children[i].classList.remove('card-clicked');
+	}
+	clearTimeout(showCards);
+}, 2000);
